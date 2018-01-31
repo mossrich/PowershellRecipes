a00fd7055715abed098f830d75da6e5f3da0b2ce
mossrich <35710431+mossrich@users.noreply.github.com>
2018-01-31 09:20:17 -0800

    styling changes and comments for clarity

--- DatagridViewFiles.ps1
+++ DatagridViewFiles.ps1
@@ -1,9 +1,10 @@
 ﻿[reflection.assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
 [reflection.assembly]::LoadWithPartialName("System.Drawing") | Out-Null
-$list = New-Object System.collections.ArrayList
 $BaseFolder = "c:\users\$($env:UserName)\Favorites"
 $allFiles = Get-ChildItem $BaseFolder -Recurse 
-$files = $allFiles  | Select @{n='Folder';e={$_.DirectoryName.Replace($BaseFolder, ".")}}, Name, LastWriteTime, Length | Sort Folder, Name #| Group -Property Folder #-AsHashTable -AsString
+$files = $allFiles  | Select @{n='Folder';e={$_.DirectoryName.Replace($BaseFolder, ".")}}, Name,LastWriteTime, Length | Sort Folder, Name #| Group -Property Folder #-AsHashTable -AsString
+
+$list = New-Object System.collections.ArrayList
 $list.AddRange($files) #$list | Out-GridView
 $form = New-Object System.Windows.Forms.Form -Property @{Size = New-Object System.Drawing.Size(900,600)}
 $Script:dataGridView = New-Object System.Windows.Forms.DataGridView -Property @{
@@ -14,26 +15,29 @@ $Script:dataGridView = New-Object System.Windows.Forms.DataGridView -Property @{
     AutoSizeColumnsMode = "AllCells"
     DataSource = $list
 }
+
 $checkCol = New-Object System.Windows.Forms.DataGridViewCheckBoxColumn -Property @{Name = 'Check';Width = 40}
 $Script:dataGridView.Columns.Add($checkCol)
-$Script:dataGridView.add_CellPainting({ Param($sender,$e)     
-    Write-host "Painting [$($e.columnIndex),$($e.rowIndex)] : current value: $($Script:dataGridView[$e.columnIndex,$e.rowIndex].Value)" 
+$Script:dataGridView.DefaultCellStyle.Padding = 0  
+$Script:dataGridView.AutoSizeRowsMode = "None"
+$Script:dataGridView.RowTemplate.Height = ($Script:dataGridView.RowTemplate.Height - 4)
+
+$Script:dataGridView.add_CellPainting({ Param($sender,$e) #Merge rows 
     $e.AdvancedBorderStyle.Bottom = [System.Windows.Forms.DataGridViewAdvancedCellBorderStyle]::None
     $e.AdvancedBorderStyle.Top = $Script:dataGridView.AdvancedCellBorderStyle.Top
     Try{
-        If( ($e.rowIndex -gt 1)  ){ #-and ($e.colIndex -gt 0)
+        If( ($e.rowIndex -gt 0)  ){ #-and ($e.colIndex -gt 0)
             $thisCell = $Script:dataGridView[$e.columnIndex,$e.rowIndex]
             $prevCell = $Script:dataGridView[$e.columnIndex,($e.rowIndex - 1)]
-            If (($prevCell -ne $null) -and ($thisCell.Value -eq $prevCell.Value)){
+            If (($prevCell -ne $null) -and ($thisCell.Value -eq $prevCell.Value )){ #hide if same as last
                 $e.AdvancedBorderStyle.Top = [System.Windows.Forms.DataGridViewAdvancedCellBorderStyle]::None
                 $Script:dataGridView[$e.columnIndex,$e.rowIndex].Style.ForeColor = [System.Drawing.Color]::Transparent
             }
         }
-    }Catch{
+    }Catch{#prevents showing a dialog for every cell if there are errors. 
+        Write-host "Painting [$($e.columnIndex),$($e.rowIndex)] : current value: $($Script:dataGridView[$e.columnIndex,$e.rowIndex].Value)" 
         Write-Error $_.Exception.Message
     } 
 })
-#$Script:dataGridView.add_DatasourceChanged({ AutoResizeColumns $frm.Controls["dg"] } )
-#$Script:dataGridView.add_VisibleChanged({ AutoResizeColumns $frm.Controls["dg"] } )
 $form.Controls.Add($Script:dataGridView)
 $form.ShowDialog()
