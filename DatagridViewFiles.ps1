db4629cd2c65106d5d78ce81964744700c6f52b1
mossrich <35710431+mossrich@users.noreply.github.com>
2018-02-07 10:23:33 -0800

    Minor refactor - only show files, not folders

--- DatagridViewFiles.ps1
+++ DatagridViewFiles.ps1
@@ -1,11 +1,11 @@
-Param([string] $BaseFolder = "c:\users\$($env:UserName)\Favorites\links")
+﻿Param([string] $BaseFolder = "c:\users\$($env:UserName)\Favorites")
 [reflection.assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
 [reflection.assembly]::LoadWithPartialName("System.Drawing") | Out-Null
 $Script:CurrentFolder = $BaseFolder
-$FilesToJSON = {Param ($folder = $Script:CurrentFolder);Get-ChildItem $folder -Recurse | Select @{n='Folder';e={$_.DirectoryName -replace [regex]::Escape($folder), "."}}, Name, LastWriteTime, Length | Sort Folder, Name | ConvertTo-Json}
-$Script:CurrentJSON = & $FilesToJSON #store as JSON for smaller memory footprint
+$FilesToJSON = {Param ($folder = $Script:CurrentFolder);Get-ChildItem $folder -Recurse -File| Select @{n='Folder';e={$_.DirectoryName -replace [regex]::Escape($folder), "."}}, Name, LastWriteTime, Length | Sort Folder, Name | ConvertTo-Json}
+$Script:CurrentJSON = & $FilesToJSON #store files as JSON for smaller memory footprint
 
-[int]$FormWidth = 1050; [int]$FormHeight = 600 #TODO: store these in registry on form-resive event and read here
+[int]$FormWidth = 1100; [int]$FormHeight = 600 #TODO: store these in registry on form-resize event and read here
 $Script:form = New-Object System.Windows.Forms.Form -Property @{Size = New-Object System.Drawing.Size($FormWidth,$FormHeight);Text = $BaseFolder}
 
 $Script:timer = New-Object System.Windows.Forms.Timer -Property @{Interval = 1000} #wait 1000 ms after updating folder name before refresh grid 
@@ -16,7 +16,7 @@ $Script:timer.add_Tick({
     $Script:dataGridView.Refresh()
 }) 
 
-$txtFolder =  New-Object System.Windows.Forms.TextBox -Property @{Name="txtFolder"; Text=$BaseFolder; Anchor="Top,Left,Right"}
+$txtFolder =  New-Object System.Windows.Forms.TextBox -Property @{Name="txtFolder"; Text=$BaseFolder; Anchor="Top,Left,Right"}  #TODO: Multi-line https://stackoverflow.com/questions/73110/how-can-i-show-scrollbars-on-a-system-windows-forms-textbox-only-when-the-text-d
 $txtFolder.add_TextChanged({Param([System.Windows.Forms.TextBox]$sender, $e)
     $Script:timer.Stop() #stop any pending update - the text box is changed
     If($sender.Text.TrimEnd('\') -ne $Script:CurrentFolder.TrimEnd('\')){ #adding a \ to the end of a folder name is still the same folder on disk, but resolves differently
@@ -32,30 +32,30 @@ $Script:dataGridView = New-Object System.Windows.Forms.DataGridView -Property @{
     Anchor = "Left,Right,Top,Bottom"
     AutoSizeColumnsMode = "AllCells" #[System.Windows.Forms.DataGridViewAutoSizeColumnMode]::AllCells - doesn't work
     AutoSizeRowsMode = "None"
-    DataSource = [System.collections.ArrayList] ((& $FilesToJSON) | ConvertFrom-Json)
+    DataSource = [System.collections.ArrayList] ((& $FilesToJSON) | ConvertFrom-Json)  # https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/differences-between-the-windows-forms-datagridview-and-datagrid-controls    The only feature that is available in the DataGrid control that is not available in the DataGridView control is the hierarchical display of information from two related tables in a single control.
     Size = New-Object System.Drawing.Size(($FormWidth - 30),($FormHeight - $txtFolder.Size.Height - 50)) 
     Location = New-Object System.Drawing.Point(0,($txtFolder.Size.Height))
 }
 #add checkbox and set styles for this instance
-$Script:dataGridView.Columns.Add( (New-Object System.Windows.Forms.DataGridViewCheckBoxColumn -Property @{Name = 'Check'}) )
+$Script:dataGridView.Columns.Add( (New-Object System.Windows.Forms.DataGridViewCheckBoxColumn -Property @{Name = 'Check'}) ) #TODO: Add combobox? https://stackoverflow.com/questions/17354173/datagridview-convert-textboxcolumn-to-comboboxcolumn
 $Script:dataGridView.RowTemplate.Height = ($Script:dataGridView.RowTemplate.Height - 4) #shrink default height by 4 pixels  #$Script:dataGridView.DefaultCellStyle.Padding = 0  #already default
 $txtFolder.Size = New-Object System.Drawing.Size(($FormWidth / 2),$txtFolder.Size.Height)
 #$Script:dataGridView.AdvancedCellBorderStyle = New-Object System.Windows.Forms.DataGridViewAdvancedBorderStyle -Property @{Bottom = [System.Windows.Forms.DataGridViewAdvancedCellBorderStyle]::NoneTop = $Script:dataGridView.AdvancedCellBorderStyle.Top} #read only property
 
 $Script:dataGridView.add_CellPainting({#Merge rows
-    Param([System.Windows.Forms.DataGridView]$sender, [System.Windows.Forms.DataGridViewCellPaintingEventArgs] $e)  
+    Param([System.Windows.Forms.DataGridView] $sender, [System.Windows.Forms.DataGridViewCellPaintingEventArgs] $e)  
     $e.AdvancedBorderStyle.Bottom = [System.Windows.Forms.DataGridViewAdvancedCellBorderStyle]::None #TODO: set cell template with these borders - need to create an inherited dgview
-    $e.AdvancedBorderStyle.Top = $Script:dataGridView.AdvancedCellBorderStyle.Top
+    $e.AdvancedBorderStyle.Top = $sender.AdvancedCellBorderStyle.Top
     Try{
         If(($e.rowIndex -le 0 ) -or ($e.columnIndex -le 0)){ return }
-        $thisCell = $Script:dataGridView[$e.columnIndex,$e.rowIndex]
-        $prevCell = $Script:dataGridView[$e.columnIndex,($e.rowIndex - 1)]
+        $thisCell = $sender[$e.columnIndex,$e.rowIndex]
+        $prevCell = $sender[$e.columnIndex,($e.rowIndex - 1)]
         If (($thisCell.Value -eq $prevCell.Value )){ #hide if same as last ($prevCell -ne $null) -and 
             $e.AdvancedBorderStyle.Top = [System.Windows.Forms.DataGridViewAdvancedCellBorderStyle]::None
-            $Script:dataGridView[$e.columnIndex,$e.rowIndex].Style.ForeColor = [System.Drawing.Color]::Transparent
+            $sender[$e.columnIndex,$e.rowIndex].Style.ForeColor = [System.Drawing.Color]::Transparent
         }
     }Catch{#prevents showing a dialog for every cell if there are errors. 
-        Write-host "Painting [$($e.columnIndex),$($e.rowIndex)] : current value: $($Script:dataGridView[$e.columnIndex,$e.rowIndex].Value)" 
+        Write-host "Painting [$($e.columnIndex),$($e.rowIndex)] : current value: $($sender[$e.columnIndex,$e.rowIndex].Value)" 
         Write-Error $_.Exception.Message
     } 
 })
