9c8d065b875792ede8be8d401dac0a136f1a5ce8
mossrich <35710431+mossrich@users.noreply.github.com>
2018-02-09 12:09:34 -0800

    First working version - accepts list parameter and groups by first column
    This script should work without modification on a Windows system with WPF and a recent Powershell installed 
    This grid will accept a sorted $datalist on the pipeline and will group by a column
    The grouping column is hidden to keep the grid tidy
    Defaults are useful for demonstration
      * $RootFolder - if no list is specified this will be used to generate a file list. C:\Windows folder takes a few minutes to load
      * $DataList - this must be castable to [System.Windows.Data.ListCollectionView] and should be sorted at least by the $GroupColumn
      * $GroupColumn - the name of the column in the $DataList that should be used for Grouping 
      * $Title, $StartExpanded, $FormWidth and $FormHeight are injected into the XAML form definition. This is kinda weird, but my first WPF. 
    
    Design notes: 
    Note that a file hierarchy is flattened by design - you can see all subfolders at the same level as parent folders, but the parent folder is shown in the group header as (>) grandparent\parent\group. This is due to my own preference and lack of ideas for displaying deep nesting well.  
    Also empty folders are not displayed due to the -File parameter of Get-ChildItem. That was not necessarily by design, but consistent with the file (or leaf) oriented intended use to manage playlists of thousands of MP3 files.  
    I chose Powershell because it's terse, performs well, and fits a lot of functionality in a single page of understandable code. 
    
    Future plans for update: 
    * Reset $RootFolder: There should be a UI to set a shown folder to the root, or to pick another parent folder. 
    * Detail-Fill:  The $DataList is intended to be a scaffold that loads and generates the grid quickly from a set of objects (files) without reading detail at load time.  The intention is to load object-specific details (ID3 tags, TFS status, etc.) in the background and prioritize load for objects that are being painted.  Or more simply, wouldn't it be nice to expand a folder and see ID3 tags being filled in while you watch? The *detail-fill* process can be any slow-loading data source: file contents, internet lookup, registry read, etc. I'm considering making the detail-fill be a script-block parameter. 
    * Playlist: Add a selected file/object to an in-memory list - maybe displayed on the form, in a collapsible window pane with a summary thumb "13 selected - 20 minutes playtime - 4.5GB"  There should be options to save the playlist, or send to with a player,  These options ("verbs") may be an array of ScriptBlocks specified as parameters. 
    * Save $DataList: Once we've loaded all the slow data, it would be better to persist it somewhere so it doesn't need to refresh every time. I've considered a JSON database because Powershell handles deserializing JSON easily. Each object's paint event should queue up a fast refresh - compare to system of record (filesystem, TFS, RSS, etc.) and confirm that the object still exists. If not, remove from the grid or otherwise indicate it's stale/dirty. Queued behind the fast refresh should be a slow refresh - confirm that the details are still up to date. Stale objects should be removed from a playlist. 
    * Tiles instead of rows: I like newspaper columns for eliminating unnecessary whitespace.  HTML tables and DIV tags would work better than WPF for grouping and styling. I already have POC code for putting these objects into defined-width tiles in newspaper columns, but not yet for reading files and organizing in groups. This Powershell script was intended to collect design ideas without adding library code and will eventually be abandoned.

---
+++ GroupableGrid.ps1
@@ -0,0 +1,64 @@
+﻿#Uses WPF to display a grouped datagrid - allows user to expand/collapse a group (group=folder if the default $datalist is used)
+param(
+    $RootFolder = "c:\users\$($env:UserName)", 
+    [Parameter(Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+    $DataList = (Get-ChildItem $RootFolder -Recurse -File -ErrorAction ignore | Select @{n='Folder';e={$_.DirectoryName -replace [regex]::Escape($RootFolder), "."}}, Name, LastWriteTime, Length | Sort Folder, Name),
+    [string] $GroupColumn = ($DataList | Get-Member -MemberType NoteProperty | Select -First 1).Name, #first column name - whatever you group by should be sorted first
+    [string] $Title = "$RootFolder",
+    [string] $StartExpanded = "False",
+    $FormWidth = 1100,
+    $FormHeight = 600 
+)
+$xaml =@"
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        x:Name="Window_GuiManagement" Title="$Title" WindowStartupLocation = "CenterScreen" 
+        Width = "$FormWidth" Height = "$FormHeight" Visibility="Visible" WindowStyle="ToolWindow" ResizeMode="CanResize" Topmost="True">
+    <Grid>
+        <DataGrid x:Name="DGItems" Margin="10,10,10,0" VerticalAlignment="Top" SelectionMode="Single" AlternationCount="1" AutoGenerateColumns="True">
+            <DataGrid.GroupStyle>
+                <GroupStyle>
+                    <GroupStyle.ContainerStyle>
+                        <Style TargetType="{x:Type GroupItem}">
+                            <Setter Property="Template">
+                                <Setter.Value>
+                                    <ControlTemplate TargetType="{x:Type GroupItem}">
+                                        <Expander IsExpanded="$StartExpanded" >
+                                            <Expander.Header>
+                                                <DockPanel>
+                                                    <TextBlock Text="{Binding Path=Name}" />
+                                                </DockPanel>
+                                            </Expander.Header>
+                                            <Expander.Content>
+                                                <ItemsPresenter />
+                                            </Expander.Content>
+                                        </Expander>
+                                    </ControlTemplate>
+                                </Setter.Value>
+                            </Setter>
+                        </Style>
+                    </GroupStyle.ContainerStyle>
+                </GroupStyle>
+            </DataGrid.GroupStyle>
+        </DataGrid>
+    </Grid>
+</Window>
+"@
+#Add-Type –assemblyName WindowsBase  #Add-Type –assemblyName PresentationCore  #[reflection.assembly]::LoadWithPartialName("System.Windows.Controls") #[reflection.assembly]::LoadWithPartialName("System..Collections.Generic") 
+Add-Type -AssemblyName presentationframework
+[reflection.assembly]::LoadWithPartialName("System.Windows.Data") #| Out-Null
+
+$reader = (New-Object System.Xml.XmlNodeReader ([xml] $xaml))
+[System.Windows.Window] $Form = [Windows.Markup.XamlReader]::Load( $reader )
+[System.Windows.Controls.DataGrid] $DG = $Form.FindName("DGItems")
+
+[System.Windows.Data.ListCollectionView] $cv = [System.Windows.Data.ListCollectionView]$DataList
+$cv.GroupDescriptions.Add((new-object System.Windows.Data.PropertyGroupDescription $GroupColumn))
+$DG.ItemsSource = $cv
+
+$DG.add_Loaded({param([System.Windows.Controls.DataGrid]$sender, $e) #hide grouped column - TODO: allow regrouping
+    ForEach($col in $sender.Columns) { 
+        $col.Visibility = If($col.Header -eq $GroupColumn){ [System.Windows.Visibility]::Hidden } else { [System.Windows.Visibility]::Visible }
+    }
+})
+$Form.ShowDialog() | out-null
