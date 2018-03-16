216fd07b53bc7a8f9a8bff9e7afe4b0397bcda53
mossrich <35710431+mossrich@users.noreply.github.com>
2018-03-16 09:38:40 -0700

    BugFix: continue with operation if a file is in use
    Added Try-Catch block around Zip operation to support continuing when a log file is in use and can't be archived. The file will be skipped and not archived.

--- ArchiveOldLogs.ps1
+++ ArchiveOldLogs.ps1
@@ -1,10 +1,8 @@
-#Sends $FileSpecs files to a zip archive if they match $Filter - deleting the original if $DeleteAfterArchiving is true. 
-#Files that have already been archived will be ignored. 
 param (
    [string] $ParentFolder = "$PSScriptRoot", #Files will be stored in the zip with path relative to this folder
    [string[]] $FileSpecs = @("*.log","*.txt","*.svclog","*.log.*"), 
    $Filter = { $_.LastWriteTime -lt (Get-Date).AddDays(-7)}, #a Where-Object function - default = older than 7 days
-   [string] $ZipPath = "$PSScriptRoot\archive-$(get-date -f yyyy-MM).zip", #create one archive per run-month - it may contain older files 
+   [string] $ZipPath = "$PSScriptRoot\archive-$(get-date -f yyyy-MM).zip", 
    [System.IO.Compression.CompressionLevel]$CompressionLevel = [System.IO.Compression.CompressionLevel]::Optimal, 
    [switch] $DeleteAfterArchiving = $true,
    [switch] $Verbose = $true,
@@ -21,14 +19,19 @@ Try{
     $WriteArchive = [IO.Compression.ZipFile]::Open( $ZipPath, [System.IO.Compression.ZipArchiveMode]::Update)
     ForEach ($File in $FileList){
         Write-Progress -Activity "Archiving old files" -Status  "Archiving file $($totalcount - $countdown) of $totalcount : $($File.Name)"  -PercentComplete (($totalcount - $countdown)/$totalcount * 100)
-        $RelativePath = (Resolve-Path -LiteralPath "$($File.FullName)" -Relative).TrimStart(".\")
-        $AlreadyArchivedFile = ($WriteArchive.Entries | Where-Object {#zip will store multiple copies of the exact same file - prevent this by checking if already archived. 
+        $RelativePath = (Resolve-Path $File.FullName -Relative).TrimStart(".\")
+        $AlreadyArchivedFile = ($WriteArchive.Entries | Where-Object {
                 (($_.FullName -eq $RelativePath) -and ($_.Length -eq $File.Length) )  -and 
-                ([math]::Abs(($_.LastWriteTime.UtcDateTime - $File.LastWriteTimeUtc).Seconds) -le 2) #ZipFileExtensions timestamps are only precise within 2 seconds. 
+                (($_.LastWriteTime.UtcDateTime - $File.LastWriteTimeUtc) -lt (New-TimeSpan -Seconds 2)) #ZipFileExtensions timestamps are only precise within 2 seconds. TODO: does this work for + or - 2 seconds?
             })     
         If($AlreadyArchivedFile -eq $null){
             If($Verbose){Write-Host "Archiving $RelativePath $($File.LastWriteTimeUtc -f "yyyyMMdd-HHmmss") $($File.Length)" }
-            $ArchivedFile = [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($WriteArchive, $File.FullName, $RelativePath, $CompressionLevel)
+            Try{
+                $ArchivedFile = [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($WriteArchive, $File.FullName, $RelativePath, $CompressionLevel)
+            }Catch{
+                Write-Warning  "$($File.FullName) could not be archived. `n $($_.Exception.Message)"  
+                $skipped = $skipped + 1
+            }
             If($File.LastWriteTime.IsDaylightSavingTime()){#HACK: fix for buggy date - adds an hour inside archive when the zipped file was created during PDT (files created during PST are not affected).  Not sure how to introduce DST attribute to file date in the archive. 
                 $entry = $WriteArchive.GetEntry($RelativePath)    
                 $entry.LastWriteTime = ($File.LastWriteTime.ToLocalTime() - (New-TimeSpan -Hours 1)) #TODO: This is better, but maybe not fully correct. Does it work in all time zones?
@@ -38,14 +41,18 @@ Try{
             $skipped = $skipped + 1
         }
         If((($ArchivedFile -ne $null) -and ($ArchivedFile.FullName -eq $RelativePath)) -and $DeleteAfterArchiving) { #delete original if it's been successfully archived. 
-            Remove-Item $File.FullName -Verbose
+            Try {
+                Remove-Item $File.FullName -Verbose:$Verbose
+            }Catch{
+                Write-Warning "$($File.FullName) could not be deleted. `n $($_.Exception.Message)"
+            }
         } 
         $countdown = $countdown -1
     }
 }Catch [Exception]{
     Write-Error $_.Exception
 }Finally{
-    $WriteArchive.Dispose() #close the zip file so it can be read later 
+    $WriteArchive.Dispose()
     Write-Host "Sent $($totalcount - $countdown - $skipped) of $totalcount files to archive: $ZipPath"
 }
 Pop-Location
