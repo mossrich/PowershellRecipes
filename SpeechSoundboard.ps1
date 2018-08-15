9aea936d6715c2c098c75304bbaaa849f5a3c006
mossrich <35710431+mossrich@users.noreply.github.com>
2018-08-15 10:28:27 -0700

    Added comments and Alt-characters
    Also reformatted the word list for brevity. 
    An & before a character will allow you to say "H&ello" with Alt-E

--- SpeechSoundboard.ps1
+++ SpeechSoundboard.ps1
@@ -1,20 +1,17 @@
-Param($WordList = @(
-        "Hello"
-        "Hi"
-        "Goodbye"
-        "I don't understand"
-        "Wait"
+#demonstrates speech synthesis with a simple sound board of common phrases
+Param($WordList = @( # & in front of a letter allows saying "H&ello" with Alt-E, if Alt-E is not already assigned. 
+        "H&ello", "&Hi", "&Goodbye", "I don't &understand", "I don't &know", "&Wait", "&Yes", "&No", "&OK"
     )
 )
 Add-Type -AssemblyName System.Speech
-$Synth = New-Object System.Speech.Synthesis.SpeechSynthesizer -Property @{rate = -1; volume = 100}
+$Synth = New-Object System.Speech.Synthesis.SpeechSynthesizer -Property @{rate = -1; volume = 100} 
 
 Add-Type -AssemblyName System.Windows.Forms
 $form1 = New-Object System.Windows.Forms.form -Property @{ShowInTaskbar = $True; WindowState = "Normal"}
 $top = 5
 $WordList|%{
     $btn = New-Object System.Windows.Forms.Button -property @{Text = $_; Top = $top}
-    $btn.add_Click({$Synth.Speak($this.Text)})
+    $btn.add_Click({$Synth.Speak($this.Text.Replace('&',''))})
     $form1.Controls.Add($btn)
     $top += $btn.Height + 5
     $form1.Height = $top + 40 
