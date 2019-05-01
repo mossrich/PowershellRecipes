25f154f3f3efd88603bb220f58713200d3e5cdc0
mossrich <35710431+mossrich@users.noreply.github.com>
2019-05-01 11:01:08 -0700

    Center menu, move vars to parameters

--- AsciiMenu.ps1
+++ AsciiMenu.ps1
@@ -5,58 +5,59 @@
 │third           │
 └────────────────┘
 #>
-function Show-SimpleMenu ([array]$MenuOptions, [string]$Title ='Choose an option',$border = '┌─┐│└┘'){
-    $maxLength = ($MenuOptions | Measure-Object -Maximum -Property Length).Maximum #get longest string length
+function Show-SimpleMenu ([array]$Options, [string]$Title ='Choose an option',$border = '┌─┐│└┘',$highlighted = 0){
+    $maxLength = ($Options | Measure-Object -Maximum -Property Length).Maximum #get longest string length
     If($maxLength -lt $Title.Length){$maxLength = $Title.Length}
-    $highlighted = 0 
     $MenuTop = [Console]::CursorTop
     Do{
         [Console]::CursorTop = $MenuTop
-        Write-Host "$($border[0])$($Title.PadRight($maxLength,$border[1]))$($border[2])" #top border: ┌Title─┐
-        for ($i = 0; $i -lt $MenuOptions.Length;$i++) {
+        #Left-align title: Write-Host "$($border[0])$($Title.PadRight($maxLength,$border[1]))$($border[2])" #top border: ┌Title─┐
+        $LeftPad = [string]$border[1] * [Math]::Max(0,[math]::Floor(($maxlength-$Title.Length)/2)) 
+        Write-Host "$($border[0])$(($LeftPad + $Title).PadRight($maxLength,$border[1]))$($border[2])"
+        for ($i = 0; $i -lt $Options.Length;$i++) {
             Write-Host $border[3] -NoNewLine
             if ($i -eq $highlighted) {
-                Write-Host ([string]$MenuOptions[$i]).PadRight($maxLength,' ') -fore ([Console]::BackgroundColor) -back ([Console]::ForegroundColor) -NoNewline
+                Write-Host ([string]$Options[$i]).PadRight($maxLength,' ') -fore ([Console]::BackgroundColor) -back ([Console]::ForegroundColor) -NoNewline
             } else {
-                Write-Host ([string]$MenuOptions[$i]).PadRight($maxLength,' ') -fore ([Console]::ForegroundColor) -back ([Console]::BackgroundColor) -NoNewline
+                Write-Host ([string]$Options[$i]).PadRight($maxLength,' ') -fore ([Console]::ForegroundColor) -back ([Console]::BackgroundColor) -NoNewline
             }
             Write-Host $border[3] 
         }
         Write-Host "$($border[4])$([string]$border[1] * $maxLength)$($border[5])" #bottom border:└─┘
         $key = [Console]::ReadKey($true).Key
         If ($key -eq [ConsoleKey]::UpArrow -and $highlighted -gt 0 ) {$highlighted--}
-        If ($key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $MenuOptions.Length - 1) {$highlighted++}
+        If ($key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $Options.Length - 1) {$highlighted++}
         If ((1..9).Contains($key.value__ - [ConsoleKey]::NumPad0.value__) -and #change highlight with 1..9 
-            ($MenuOptions.Length + [ConsoleKey]::NumPad1.value__ -gt $key.value__)) { 
+            ($Options.Length + [ConsoleKey]::NumPad1.value__ -gt $key.value__)) { 
                 $highlighted = $key.value__ - [ConsoleKey]::NumPad1.value__ 
         }
     }While($key -ne [ConsoleKey]::Enter -and $key -ne [ConsoleKey]::Escape )
-    If($key -eq [ConsoleKey]::Enter){ $MenuOptions[$highlighted] }
+    If($key -eq [ConsoleKey]::Enter){ $Options[$highlighted] }
 }
 
 <#
 Shows an ascii menu: highlight with up/down arrows, select with space bar and choose with [Enter]. 
-┌─Select with spacebar┐
-│√first               │
-│ second option       │
-│√third               │
-└─────────────────────┘
+┌─Select with spacebar─┐
+│√first                │
+│ second option        │
+│√third                │
+└──────────────────────┘
 #>
-function Show-MultiSelectMenu ([array]$MenuOptions, [string]$Title ='Select with spacebar', $border = '┌─┐│└┘'){
-    $maxLength = ($MenuOptions | Measure-Object -Maximum -Property Length).Maximum + 1 #get longest string length, +1 padding
-    If($maxLength -lt $Title.Length){$maxLength = $Title.Length + 1}
-    $highlighted = 0 
-    $selected = New-Object bool[] $MenuOptions.Length #defaults to $false
+function Show-MultiSelectMenu ([array]$Options, [string]$Title ='Select with spacebar', $border = '┌─┐│└┘',
+            $highlighted = 0, $selected = (New-Object bool[] $Options.Length ) ){
+    $maxLength = ($Options | Measure-Object -Maximum -Property Length).Maximum + 1 #get longest string length, +padding for √ 
+    If($maxLength -lt $Title.Length + 2){$maxLength = $Title.Length + 1}
     $MenuTop = [Console]::CursorTop
     Do{
         [Console]::CursorTop = $MenuTop
-        Write-Host "$($border[0])$($border[1])$($Title.PadRight($maxLength,$border[1]))$($border[2])" #top border: ┌─Title─┐
-        for ($i = 0; $i -lt $MenuOptions.Length;$i++) {#draw the menu
+        $LeftPad = [string]$border[1] * [Math]::Max(1,[math]::Floor(($maxlength-$Title.Length)/2)) #Centered, at least one border ─
+        Write-Host "$($border[0])$(($LeftPad + $Title).PadRight($maxLength + 1,$border[1]))$($border[2])" #top border: ┌─Title─┐
+        for ($i = 0; $i -lt $Options.Length;$i++) {#draw the menu
             If($selected[$i]){ Write-Host "$($border[3])√" -NoNewLine }else{ Write-Host "$($border[3]) " -NoNewLine }
             if ($i -eq $highlighted) {
-                Write-Host ([string]$MenuOptions[$i]).PadRight($maxLength,' ') -fore ([Console]::BackgroundColor) -back ([Console]::ForegroundColor) -NoNewline
+                Write-Host ([string]$Options[$i]).PadRight($maxLength,' ') -fore ([Console]::BackgroundColor) -back ([Console]::ForegroundColor) -NoNewline
             } else {
-                Write-Host ([string]$MenuOptions[$i]).PadRight($maxLength,' ') -fore ([Console]::ForegroundColor) -back ([Console]::BackgroundColor) -NoNewline
+                Write-Host ([string]$Options[$i]).PadRight($maxLength,' ') -fore ([Console]::ForegroundColor) -back ([Console]::BackgroundColor) -NoNewline
             }
             Write-Host $border[3]
         }
@@ -64,19 +65,16 @@ function Show-MultiSelectMenu ([array]$MenuOptions, [string]$Title ='Select with
         $key = [Console]::ReadKey($true).Key 
         If ($key -eq [ConsoleKey]::Spacebar) {$selected[$highlighted] = !$selected[$highlighted] }
         If ($key -eq [ConsoleKey]::UpArrow -and $highlighted -gt 0 ) {$highlighted--}
-        If ($key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $MenuOptions.Length - 1) {$highlighted++}
+        If ($key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $Options.Length - 1) {$highlighted++}
         If ((1..9).Contains($key.value__ - [ConsoleKey]::NumPad0.value__) -and 
-            ($MenuOptions.Length + [ConsoleKey]::NumPad1.value__ -gt $key.value__)) { 
+            ($Options.Length + [ConsoleKey]::NumPad1.value__ -gt $key.value__)) { 
                 $highlighted = $key.value__ - [ConsoleKey]::NumPad1.value__ #change highlight with 1..9 
         }
     }While(! @([ConsoleKey]::Enter, [ConsoleKey]::Escape ).Contains($key)) #stop if Enter or Esc
     If($key -eq [ConsoleKey]::Enter){ #return the menu options that are selected
-        $MenuOptions | %{$i=0}{#TIL: foreach can have a 'begin' scriptbock that's executed only once
-            If($selected[$i]){$_}
-            $i++
-        }
+        $Options | %{$i=0}{ If($selected[$i++]){$_} } #TIL: foreach can have a 'begin' scriptbock that's executed only once
     }
 }
-
-Show-SimpleMenu @('first','second option','third') #-border '╔═╗║╚╝'
-Show-MultiSelectMenu @('first','second, much longer option','third') #-border '╔═╗║╚╝'
+$doubleBorder = '╔═╗║╚╝'
+Show-SimpleMenu @('first','second longer option','third') -border $doubleBorder
+Show-MultiSelectMenu @('first','second','third')  -selected @($true,$false,$true) #if $selected is shorter than $options, throws error selecting the last
