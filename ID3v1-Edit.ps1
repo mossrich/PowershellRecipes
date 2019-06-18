00caa2fa181837df16f370a465bf10825f236552
mossrich <35710431+mossrich@users.noreply.github.com>
2019-06-18 15:34:17 -0700

    Added Get-ID3v2 that parses some ID3v2 frames
    TODO: 
    * Works ok for Win7 sample MP3s - not tested on others
    * Not all frame types are parsed - unmapped ones will show a warning message
    * There is special parsing for some frame types that I don't understand well - APIC needs work
    * Need a cleaner mapping to Frame Type name, and maybe add a property for 4-letter frame type

--- ID3v1-Edit.ps1
+++ ID3v1-Edit.ps1
@@ -83,7 +83,69 @@ Function Set-ID3v1( #All parameters except path are optional, they will not chan
     }
     If($BackDate){(Get-ChildItem -LiteralPath $path).LastWriteTime = $CurrentModified.AddMinutes(1)}
 }
-$path = (Get-ChildItem (Join-path $env:USERPROFILE "*.mp3") -Recurse | Select -First 1).FullName
+<#$path = (Get-ChildItem (Join-path $env:USERPROFILE "*.mp3") -Recurse | Select -First 1).FullName
 Get-ID3v1 $path
 Set-ID3v1 -path $path -Year 1996
 Get-ID3v1 $path
+#>
+
+Function Get-4ByteSize($Bytes){
+    #size=data[6]*(1<<21)+data[7]*(1<<14)+data[8]*(1<<7)+data[9];https://github.com/sahands/a-id3/blob/master/ID3/Source/Tag/TagHeader.cs
+    $Bytes[0] * (1 -shl 21) + $Bytes[1] * (1 -shl 14) + $Bytes[2] * (1 -shl 7) + $Bytes[3] #TODO: why is this 1/2 size for APIC 
+}
+
+$FrameMap = @{#subset of id3.org 
+   'APIC' = [psobject]@{Description = 'Attached picture'; Parser={}}
+   'COMM' = [psobject]@{Description = 'Comments'; Parser={}}
+   'GEOB' = [psobject]@{Description = 'General encapsulated object'; Parser={}}
+   'PRIV' = [psobject]@{Description = 'Private frame'; Parser={}}
+   'TALB' = [psobject]@{Description = 'Album/Movie/Show title'; Parser={}}
+   'TCOM' = [psobject]@{Description = 'Composer'; Parser={}}
+   'TCON' = [psobject]@{Description = 'Content type'; Parser={}}
+   'TIT2' = [psobject]@{Description = 'Title/songname/content description'; Parser={}}
+   'TPE1' = [psobject]@{Description = 'Lead performer(s)/Soloist(s)'; Parser={}}
+   'TPE2' = [psobject]@{Description = 'Band/orchestra/accompaniment'; Parser={}}
+   'TPE3' = [psobject]@{Description = 'Conductor'; Parser={}}
+   'TPUB' = [psobject]@{Description = 'Publisher'; Parser={}}
+   'TRCK' = [psobject]@{Description = 'Track'; Parser={}}
+   'TYER' = [psobject]@{Description = 'Year'; Parser={}}
+}
+
+Function Get-ID3v2([parameter(ValueFromPipeline)] [string]$path){
+    Process{
+        [Text.Encoding[]] $encoders = ([Text.Encoding]::GetEncoding('ISO-8859-1'), [Text.Encoding]::Unicode) #a flag will indicate Unicode text follows 
+        $buf = New-Object byte[] (10)
+        $strm = [IO.File]::OpenRead($path) 
+        $strm.Read($buf,0,$buf.Length) | Out-Null
+        If ($encoders[0].GetString($buf[0..2]) -eq 'ID3') {#A tagged file will start with 'ID3' 
+            $ID3v2 = [ordered]@{}
+            $ID3v2['Path'] = $path
+            $ID3v2['ID3Version'] = $buf[3]
+            $TagSize = Get-4ByteSize $buf[6..9] #$buf[6] * (1 -shl 21) + $buf[7] * (1 -shl 14) + $buf[8] * (1 -shl 7) + $buf[9] 
+            $ID3v2['TagSize'] = $TagSize
+            While($strm.Position -lt $TagSize){
+                $strm.Read($buf,0,$buf.Length) | Out-Null
+                $FrameType = $encoders[0].GetString($buf[0..3])
+                $FrameSize = Get-4ByteSize $buf[4..7]  #$buf[4] * (1 -shl 21) + $buf[5] * (1 -shl 14) + $buf[6] * (1 -shl 7) + $buf[7] 
+                If($FrameMap[$FrameType] -eq $null){ #Stop reading if unrecognized frame 
+                    Write-Warning "Unrecognized frame type: $FrameType at $($strm.Position - 10) ($('{0:X}' -f  ($strm.Position - 10)))"; break 
+                }
+                If($FrameType -eq 'APIC'){ #TODO: Why is the size doubled for APIC/binary?
+                    $FrameSize = $buf[4] * (1 -shl 24) + $buf[5] * (1 -shl 16) + $buf[6] * (1 -shl 8) + $buf[7] 
+                }
+                [byte]$isUnicode = 0
+                $strm.Read($isUnicode,0,1) | Out-Null
+                $FrameBuf = New-Object byte[] ($FrameSize - 1)
+                $strm.Read($FrameBuf,0,$FrameBuf.Length) | Out-Null
+                $ID3v2[$FrameMap[$FrameType].Description] = $encoders[$isUnicode].GetString($FrameBuf)
+                Write-Verbose "Read - Frame: $FrameType Size: $FrameSize Encoding: $($encoder)  Stream Position: $($strm.Position)($('{0:X}' -f  ($strm.Position))) of $TagSize ($('{0:X}' -f  $TagSize))"
+            } 
+        }
+        $strm.Close()
+        $ID3v2
+    }
+}
+
+Get-ID3v2 'C:\Users\Public\Music\Sample Music\Sleep Away.mp3'
+Get-ID3v2 'C:\Users\Public\Music\Sample Music\Kalimba.mp3'
+Get-ID3v2 'C:\Users\Public\Music\Sample Music\Maid with the Flaxen Hair.mp3' #-Verbose
