d29f2cb7b67b4576c2edccbf3f61b7fbc25e87e7
mossrich <35710431+mossrich@users.noreply.github.com>
2019-06-27 18:17:34 -0700

    Refactored and made smaller

--- ToggleFusionLogging.ps1
+++ ToggleFusionLogging.ps1
@@ -1,27 +1,19 @@
-#Toggles the state of fusion logging on or off
-Param(
-    $LogPath = "c:\temp\Fusion"
-)
+Param( $LogPath = "c:\temp\Fusion" )
+
 #restart as admin if not started as admin 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -PassThru -Verb RunAs | Out-null; exit 
 }
-Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Fusion
-If(!(Test-Path HKLM:\SOFTWARE\Microsoft\Fusion)) {New-Item -Path HKLM:\SOFTWARE\Microsoft\Fusion -ItemType Container | Out-Null}
-$LoggingEnabled = ((Get-ItemProperty -path HKLM:\SOFTWARE\Microsoft\Fusion).EnableLog) -eq 1
-If(!$LoggingEnabled){
-    Write-Host "Enabling Fusion Logging at $LogPath"
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name EnableLog        -Value 1               -Type DWord
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name ForceLog         -Value 1               -Type DWord
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name LogFailures      -Value 1               -Type DWord
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name LogResourceBinds -Value 1               -Type DWord
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name LogPath          -Value $LogPath
-    If(!(Test-Path $LogPath -PathType Container)){New-Item $LogPath -ItemType Directory}
-}Else{
-    Write-Host "Disabling Fusion Logging"
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name EnableLog        -Value 0               -Type DWord
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name ForceLog         -Value 0               -Type DWord
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name LogFailures      -Value 0               -Type DWord
-    Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name LogResourceBinds -Value 0               -Type DWord
-}
+$RegKey = 'HKLM:\SOFTWARE\Microsoft\Fusion'
+If(!(Test-Path $RegKey)) {New-Item -Path $RegKey -ItemType Container | Out-Null}
+Push-Location $RegKey
+$NewValue = If((Get-ItemProperty $RegKey).EnableLog -eq 1){0} Else {1}
+Set-ItemProperty -Path $RegKey -Name EnableLog        -Value $NewValue -Type DWord
+Set-ItemProperty -Path $RegKey -Name ForceLog         -Value $NewValue -Type DWord
+Set-ItemProperty -Path $RegKey -Name LogFailures      -Value $NewValue -Type DWord
+Set-ItemProperty -Path $RegKey -Name LogResourceBinds -Value $NewValue -Type DWord
+Set-ItemProperty -Path $RegKey -Name LogPath          -Value $LogPath  -Type String
+If(!(Test-Path $LogPath -PathType Container)){New-Item $LogPath -ItemType Directory | Out-Null}
+Write-Host "$(If($NewValue -eq 1){'Enabled'}Else{'Disabled'}) Fusion Logging at $LogPath"
+Pop-Location
 If(!$Host.Name.Contains("ISE")){Pause}
