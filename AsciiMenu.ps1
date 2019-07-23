b6b1f856500c3bc55a82138db281f9834f24b774
mossrich <35710431+mossrich@users.noreply.github.com>
2019-07-22 17:00:02 -0700

    Refactored as single function Show-BoxMenu
    Show-SimpleMenu and Show-MultiSelectMenu functions were too similar, and had repeated code. Combined to Show-BoxMenu. 
    * Added switch -Multiselect to allow selecting with space 
    * Added switch -Expand to widen to the console width and add rows for title
    * Added -Marker param to allow changing the selected option marker
    * Cleaned up some width bugs

--- AsciiMenu.ps1
+++ AsciiMenu.ps1
@@ -1,85 +1,77 @@
-<# Shows an ascii menu: highlight with up/down arrows or 1..9, or first letter of option and choose with [Enter]. 
-┌Choose an option┐
-│first           │
-│second option   │
-│third           │
-└────────────────┘
-Note that ReadKey (required for up/down arrows) doesn't work in ISE. There is no graceful downgrade for this script. 
-#>
-function Show-SimpleMenu ([array]$Options, [string]$Title ='Choose an option',$border = '┌─┐│└┘',[int]$highlighted = 0){
-    $maxLength = ($Options + (,$Title) | Measure -Max -Prop Length).Maximum #number of chars between | and | 
-    $MenuTop = [Console]::CursorTop
-    Do{
-        [Console]::CursorTop = $MenuTop
-        $LeftPad = [string]$border[1] * [Math]::Max(0,[math]::Floor(($maxlength-$Title.Length)/2)) #gets the left padding required to center the title
-        Write-Host "$($border[0])$(($LeftPad + $Title).PadRight($maxLength,$border[1]))$($border[2])" # #top border: ┌Title─┐   left-aligned: Write-Host "$($border[0])$($Title.PadRight($maxLength,$border[1]))$($border[2])" 
-        for ($i = 0; $i -lt $Options.Length;$i++) {
-            Write-Host $border[3] -NoNewLine
-            if ($i -eq $highlighted) {
-                Write-Host ([string]$Options[$i]).PadRight($maxLength,' ') -fore ([Console]::BackgroundColor) -back ([Console]::ForegroundColor) -NoNewline
-            } else {
-                Write-Host ([string]$Options[$i]).PadRight($maxLength,' ') -NoNewline
-            }
-            Write-Host $border[3]
-        }
-        Write-Host "$($border[4])$([string]$border[1] * $maxLength)$($border[5])" #bottom border:└─┘
-        $key = [Console]::ReadKey($true)
-        If ($key.Key -eq [ConsoleKey]::UpArrow -and $highlighted -gt 0 ) {$highlighted--}
-        ElseIf ($key.Key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $Options.Length - 1) {$highlighted++}
-        ElseIf ( (1..9 -join '').contains($key.KeyChar) -and $Options.Length -ge [int]::Parse($key.KeyChar)) { $highlighted = [int]::Parse($key.KeyChar) - 1 }#change highlight with 1..9 
-        Else { 
-            (([math]::min($highlighted + 1, $Options.Length) .. $Options.Length) + (0 .. ($highlighted - 1))) | %{ #cycle from highlighted + 1 to end, and restart
-                If($Options[$_] -and $Options[$_].StartsWith($key.KeyChar) ){$highlighted = $_; Continue} #if letter matches first letter, highlight 
-            }
-        }
-    }While( -not ([ConsoleKey]::Enter,[ConsoleKey]::Escape).Contains($key.Key) )
-    If($Key.Key -eq [ConsoleKey]::Enter){ $Options[$highlighted] }
-}
-
 <#
-Shows an ascii menu: highlight with up/down arrows or 1..9, or first letter of option. Select with space bar and choose with [Enter]. 
+Shows an ASCII Box menu: highlight with up/down arrows or 1..9, or first letter of option. 
 ┌─Select with spacebar─┐
 │√first                │
 │ second               │
 │√third                │
 └──────────────────────┘
+Note that ReadKey (required for up/down arrows) doesn't work in ISE. There is no graceful downgrade for this script. 
 #>
-function Show-MultiSelectMenu ([array]$Options, [string]$Title ='Select with spacebar', $border = '┌─┐│└┘',
-            $highlighted = 0, $selected = (New-Object bool[] $Options.Length ) ){
-    $maxLength = ($Options + (,$Title) | Measure -Max -Prop Length).Maximum + 1 #get longest string length, +padding for √ 
+function Show-BoxMenu (
+        [string[]] $Options, # the list of options to choose
+        [bool[]] $Selected = (New-Object bool[] $Options.Length ), #initially selected state of options
+        [int] $Highlighted = 0, #initially highlighted option
+        [switch] $MultiSelect, #If specified space bar selects. Otherwise only the highlighted choice is returned
+        [switch] $Expand, #If specified the title will be on its own row and the menu will be as wide as the console
+        [string] $Border = '┌─┐│└┘├─┤', # '╔═╗║╚╝╟─╢' or '+-+|+++-+' also works 
+        [string] $Title = $(If($MultiSelect){'Select with spacebar'} Else {'Choose an option'}), #
+        [string] $Marker = $(If($MultiSelect){'√ '} Else {''}) #this string appears before a selected option
+  ){
+    $Width = ($Options + (,$Title) | Measure-Object -Maximum -Property Length).Maximum + ($Marker.Length * 2) #number of chars between | and | 
+    If($Expand) {$Width = [Math]::Max( $Width, [Console]::WindowWidth - 2) } # -2: leave space for border 
     If($Selected.Length -lt $Options.Length){$Selected += (New-Object bool[] ($Options.Length - $Selected.Length)) } #pad $Selected to $Options.length
     $MenuTop = [Console]::CursorTop
     Do{
         [Console]::CursorTop = $MenuTop
-        $LeftPad = [string]$border[1] * [Math]::Max(1,[math]::Floor(($maxlength-$Title.Length)/2)) #Centered, at least one border ─
-        Write-Host "$($border[0])$(($LeftPad + $Title).PadRight($maxLength + 1,$border[1]))$($border[2])" #top border: ┌─Title─┐
-        for ($i = 0; $i -lt $Options.Length;$i++) {#draw the menu
-            Write-Host "$($border[3])$(If($selected[$i]){"√"}else{" "})" -NoNewLine
-            if ($i -eq $highlighted) {
-                Write-Host ([string]$Options[$i]).PadRight($maxLength,' ') -fore ([Console]::BackgroundColor) -back ([Console]::ForegroundColor) -NoNewline
-            } else {
-                Write-Host ([string]$Options[$i]).PadRight($maxLength,' ') -NoNewline
-            }
-            Write-Host $border[3]
+        $LeftPad = [Math]::Max($Marker.Length,[math]::Floor(($Width-$Title.Length)/2)) # Left padding centers the title
+        If($Expand){ #draw a title bar as wide as the console, on its own row
+            $EOL = "`r" #If line extends to far right of console, an extra line break will be inserted - leave it off 
+            Write-Host "$($Border[0])$([string]$Border[1] * ($Width))$($Border[2])$EOL" -NoNewline                    # ┌───────┐
+            Write-Host "$($Border[3])$(((' ' * $LeftPad) + $Title).PadRight($Width,' '))$($Border[3])$EOL" -NoNewline # │ Title │
+            Write-Host "$($Border[6])$([string]$Border[7] * ($Width))$($Border[8])$EOL" -NoNewline                    # ├───────┤
+        }Else{ #Draw a narrow menu with $Title in the middle of the frame top 
+            $EOL = "`r`n" #A CR/LF is necessary except when the line is [Console]::WindowWidth long
+            Write-Host "$($Border[0])$((([string]$Border[1] * $LeftPad) + $Title).PadRight($Width,$Border[1]))$($Border[2])$EOL" -NoNewline # ┌─Title─┐
         }
-        Write-Host "$($border[4])$($border[1])$([string]$border[1] * ($maxLength))$($border[5])"
+        for ($i = 0; $i -lt $Options.Length;$i++) {#draw the menu options
+            Write-Host "$($Border[3])$(If($Selected[$i]){$Marker}else{' ' * $Marker.length})" -NoNewLine 
+            if ($i -eq $Highlighted) { #print in reverse video
+                Write-Host ([string]$Options[$i]).PadRight($Width - $marker.Length,' ') -fore ([Console]::BackgroundColor) -back ([Console]::ForegroundColor) -NoNewline
+            } else { 
+                Write-Host ([string]$Options[$i]).PadRight($Width - $marker.Length,' ') -NoNewline
+            }
+            Write-Host "$($Border[3])$EOL" -NoNewline
+        }
+        Write-Host "$($Border[4])$([string]$Border[1] * ($Width))$($Border[5])" # bottom of frame └──────┘
         $key = [Console]::ReadKey($true)
         If ($key.Key -eq [ConsoleKey]::Spacebar) {$Selected[$Highlighted] = !$Selected[$Highlighted]; If($Highlighted -lt $Options.Length - 1){$Highlighted++} }
-        ElseIf ($key.Key -eq [ConsoleKey]::UpArrow -and $highlighted -gt 0 ) {$highlighted--}
-        ElseIf ($key.Key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $Options.Length - 1) {$highlighted++}
-        ElseIf ( (1..9 -join '').contains($key.KeyChar) -and $Options.Length -ge [int]::Parse($key.KeyChar)) { $highlighted = [int]::Parse($key.KeyChar) - 1 }#change highlight with 1..9 
+        ElseIf ($key.Key -eq [ConsoleKey]::UpArrow -and $Highlighted -gt 0 ) {$Highlighted--}
+        ElseIf ($key.Key -eq [ConsoleKey]::DownArrow -and $Highlighted -lt $Options.Length - 1) {$Highlighted++}
+        ElseIf ( (1..9 -join '').contains($key.KeyChar) -and $Options.Length -ge [int]::Parse($key.KeyChar)) { $Highlighted = [int]::Parse($key.KeyChar) - 1 }#change highlight with 1..9 
         Else { 
-            (([math]::min($highlighted + 1, $Options.Length) .. $Options.Length) + (0 .. ($highlighted - 1))) | %{ #cycle from highlighted + 1 to end, and restart
-                If($Options[$_] -and $Options[$_].StartsWith($key.KeyChar) ){$highlighted = $_; Continue} #if letter matches first letter, highlight 
+            (([math]::min($Highlighted + 1, $Options.Length) .. $Options.Length) + (0 .. ($Highlighted - 1))) | %{ #cycle from highlighted + 1 to end, and restart
+                If($Options[$_] -and $Options[$_].StartsWith($key.KeyChar) ){$Highlighted = $_; Continue} #if letter matches first letter, highlight 
             }
         }
     }While(! @([ConsoleKey]::Enter, [ConsoleKey]::Escape ).Contains($key.Key)) #stop if Enter or Esc
-    If($key.Key-eq [ConsoleKey]::Enter){ #return the menu options that are selected
-        $Options | %{$i=0}{ If($selected[$i++]){$_} } #TIL: foreach can have a 'begin' scriptbock that's executed only once
+    If($key.Key-eq [ConsoleKey]::Enter){ #return the options that are selected
+        If($MultiSelect){
+            $Options | %{$i=0}{ If($Selected[$i++]){$_} } #TIL: foreach can have a 'begin' scriptbock that's executed only once
+        }Else{
+            $Options[$Highlighted]
+        }
     }
 }
-$lowASCIIBorder = '+-+|++' #are there any consoles or fonts where ASCII box borders won't show?
-$doubleBorder = '╔═╗║╚╝'
+#Backward compatibility:
+Function Show-SimpleMenu([array]$Options, [string]$Title ='Choose an option',$border = '┌─┐│└┘',[int]$highlighted = 0){ Show-BoxMenu -Options $Options -Title $Title -Border $border -Highlighted $highlighted }
+function Show-MultiSelectMenu ([array]$Options, [string]$Title ='Select with spacebar', $border = '┌─┐│└┘',
+            $highlighted = 0, $selected = (New-Object bool[] $Options.Length ) ){ Show-BoxMenu -Options $Options -Title $Title -Border $border -Highlighted $highlighted -Selected $selected -MultiSelect}
+
+$lowASCIIBorder = '+-+|+++-+' #are there any consoles or fonts where ASCII box borders won't show?
+$doubleBorder =   '╔═╗║╚╝╟─╢'
 Show-SimpleMenu @('first','second option','third','fourth','fifth') -border $doubleBorder
 Show-MultiSelectMenu @('first','second','third','fourth','fifth')  -selected @($true,$false,$true) 
 Show-MultiSelectMenu (Get-ChildItem -Path . -Directory | Select-Object -ExpandProperty FullName)  -selected @($true,$false,$true) 
+Show-BoxMenu @('first','second, very long option','third','fourth','fifth') -border $doubleBorder -Expand 
+Show-BoxMenu @('first','second option','third','fourth','fifth')  -selected @($true,$false,$true) -MultiSelect 
+Show-BoxMenu (Get-ChildItem -Path . -Directory | Select-Object -ExpandProperty FullName)  -selected @($true,$false,$true) -MultiSelect -Expand
