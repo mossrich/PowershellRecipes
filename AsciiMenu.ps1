344622ba2efb6d3f1eea2f72ba67734443fcb58a
mossrich <35710431+mossrich@users.noreply.github.com>
2019-08-02 15:36:13 -0700

    Support for paging when more options than console height
    Discovered issues when there are more options than fit on screen - the  redraws made the menu difficult to use. 
    * Added scrolling and paging support with a (somewhat-buggy) scroll thumb:  border[9]
    * Moved height/width calculation into the draw loop to allow resize on the fly
    * Added status to Bottom border:  shows the number of selected/total items if -MultiSelect is specified
    Tested with a list of 133 files. Scrolling worked, but thumb disappeared in some cases near the end of the list.

--- AsciiMenu.ps1
+++ AsciiMenu.ps1
@@ -1,5 +1,5 @@
 <#
-Shows an ASCII Box menu: highlight with up/down arrows or 1..9, or first letter of option. 
+Shows an ASCII Box menu: highlight with arrow up/down, PgUp/PgDn, 1..9, or first letter of option. 
 ┌─Select with spacebar─┐
 │√first                │
 │ second               │
@@ -13,15 +13,18 @@ function Show-BoxMenu (
         [int] $Highlighted = 0, #initially highlighted option
         [switch] $MultiSelect, #If specified space bar selects. Otherwise only the highlighted choice is returned
         [switch] $Expand, #If specified the title will be on its own row and the menu will be as wide as the console
-        [string] $Border = '┌─┐│└┘├─┤', # '╔═╗║╚╝╟─╢' or '+-+|+++-+' also works 
+        [string] $Border = '┌─┐│└┘├─┤▒', # '╔═╗║╚╝╟─╢╢' or '+-+|+++-++' also works 
         [string] $Title = $(If($MultiSelect){'Select with spacebar'} Else {'Choose an option'}), #
         [string] $Marker = $(If($MultiSelect){'√ '} Else {''}) #this string appears before a selected option
   ){
     $Width = ($Options + (,$Title) | Measure-Object -Maximum -Property Length).Maximum + ($Marker.Length * 2) #number of chars between | and | 
-    If($Expand) {$Width = [Math]::Max( $Width, [Console]::WindowWidth - 2) } # -2: leave space for border 
     If($Selected.Length -lt $Options.Length){$Selected += (New-Object bool[] ($Options.Length - $Selected.Length)) } #pad $Selected to $Options.length
-    $MenuTop = [Console]::CursorTop
-    Do{
+    $MenuTop = [Console]::CursorTop 
+    $FirstShowingOption = 0 
+    $ScrollThumbIndex = -1 
+    Do{#redraw complete menu from CursorTop
+        $MaxOptionsToShow = [Console]::WindowHeight - 3 - $(If($Expand){2}Else{0}) # can only show as many options as unused screen rows - recalculate if resized 
+        If($Expand) {$Width = [Math]::Max( $Width, [Console]::WindowWidth - 2) } # -2: leave space for border 
         [Console]::CursorTop = $MenuTop
         $LeftPad = [Math]::Max($Marker.Length,[math]::Floor(($Width-$Title.Length)/2)) # Left padding centers the title
         If($Expand){ #draw a title bar as wide as the console, on its own row
@@ -33,26 +36,32 @@ function Show-BoxMenu (
             $EOL = "`r`n" #A CR/LF is necessary except when the line is [Console]::WindowWidth long
             Write-Host "$($Border[0])$((([string]$Border[1] * $LeftPad) + $Title).PadRight($Width,$Border[1]))$($Border[2])$EOL" -NoNewline # ┌─Title─┐
         }
-        for ($i = 0; $i -lt $Options.Length;$i++) {#draw the menu options
+        for ($i = $FirstShowingOption; $i -lt [Math]::Min($Options.Length, $MaxOptionsToShow + $FirstShowingOption);$i++) {#draw as many menu options as will fit on the screen, starting from $TopOpt
             Write-Host "$($Border[3])$(If($Selected[$i]){$Marker}else{' ' * $Marker.length})" -NoNewLine 
             if ($i -eq $Highlighted) { #print in reverse video
                 Write-Host ([string]$Options[$i]).PadRight($Width - $marker.Length,' ') -fore ([Console]::BackgroundColor) -back ([Console]::ForegroundColor) -NoNewline
             } else { 
                 Write-Host ([string]$Options[$i]).PadRight($Width - $marker.Length,' ') -NoNewline
             }
-            Write-Host "$($Border[3])$EOL" -NoNewline
+            Write-Host "$(If($ScrollThumbIndex -eq $i){$Border[9]}Else{$Border[3]})$EOL" -NoNewline
         }
-        Write-Host "$($Border[4])$([string]$Border[1] * ($Width))$($Border[5])" # bottom of frame └──────┘
+        $Status = If($MultiSelect){'{0}/{1}' -f ($Selected | ?{$_ -eq $true}).Count, $Options.Length}Else{''}  #Debug -  'Th:' + $ScrollThumbIndex + ' F:' + $FirstShowingOption + ' L:' + $Options.Length  + ' M:' + $MaxOptionsToShow
+        Write-Host "$($Border[4])$($Status.PadLeft($Width,$Border[1]))$($Border[5])" -NoNewline # bottom of frame    └───────2/20┘  
         $key = [Console]::ReadKey($true)
         If ($key.Key -eq [ConsoleKey]::Spacebar) {$Selected[$Highlighted] = !$Selected[$Highlighted]; If($Highlighted -lt $Options.Length - 1){$Highlighted++} }
-        ElseIf ($key.Key -eq [ConsoleKey]::UpArrow -and $Highlighted -gt 0 ) {$Highlighted--}
-        ElseIf ($key.Key -eq [ConsoleKey]::DownArrow -and $Highlighted -lt $Options.Length - 1) {$Highlighted++}
+        ElseIf ($key.Key -eq [ConsoleKey]::UpArrow  ) {$Highlighted = [math]::Max($Highlighted - 1, 0);}
+        ElseIf ($key.Key -eq [ConsoleKey]::DownArrow) {$Highlighted = [math]::Min($Highlighted + 1, $Options.Length - 1);}
+        ElseIf ($key.Key -eq [ConsoleKey]::PageUp   ) {$Highlighted = [math]::Max($Highlighted - $MaxOptionsToShow,0); $FirstShowingOption = [math]::Max($FirstShowingOption - $MaxOptionsToShow,0)}
+        ElseIf ($key.Key -eq [ConsoleKey]::PageDown ) {$Highlighted = [math]::Min($Highlighted + $MaxOptionsToShow,$Options.Length - 1); $FirstShowingOption = [math]::Min($FirstShowingOption + $MaxOptionsToShow,$Options.Length - $MaxOptionsToShow)}
         ElseIf ( (1..9 -join '').contains($key.KeyChar) -and $Options.Length -ge [int]::Parse($key.KeyChar)) { $Highlighted = [int]::Parse($key.KeyChar) - 1 }#change highlight with 1..9 
         Else { 
             (([math]::min($Highlighted + 1, $Options.Length) .. $Options.Length) + (0 .. ($Highlighted - 1))) | %{ #cycle from highlighted + 1 to end, and restart
                 If($Options[$_] -and $Options[$_].StartsWith($key.KeyChar) ){$Highlighted = $_; Continue} #if letter matches first letter, highlight 
             }
         }
+        If($Highlighted -ge ($MaxOptionsToShow + $FirstShowingOption)){$FirstShowingOption++} #scroll if at bottom
+        If($Highlighted -lt $FirstShowingOption){$FirstShowingOption--} #scroll if at top 
+        If($MaxOptionsToShow -lt $Options.Length){$ScrollThumbIndex = [math]::Min($Options.Length - 1, [math]::Ceiling(($FirstShowingOption / ($Options.Length - $MaxOptionsToShow)) * $MaxOptionsToShow) + $FirstShowingOption)  }
     }While(! @([ConsoleKey]::Enter, [ConsoleKey]::Escape ).Contains($key.Key)) #stop if Enter or Esc
     If($key.Key-eq [ConsoleKey]::Enter){ #return the options that are selected
         If($MultiSelect){
@@ -67,11 +76,11 @@ Function Show-SimpleMenu([array]$Options, [string]$Title ='Choose an option',$bo
 function Show-MultiSelectMenu ([array]$Options, [string]$Title ='Select with spacebar', $border = '┌─┐│└┘',
             $highlighted = 0, $selected = (New-Object bool[] $Options.Length ) ){ Show-BoxMenu -Options $Options -Title $Title -Border $border -Highlighted $highlighted -Selected $selected -MultiSelect}
 
-$lowASCIIBorder = '+-+|+++-+' #are there any consoles or fonts where ASCII box borders won't show?
-$doubleBorder =   '╔═╗║╚╝╟─╢'
-Show-SimpleMenu @('first','second option','third','fourth','fifth') -border $doubleBorder
-Show-MultiSelectMenu @('first','second','third','fourth','fifth')  -selected @($true,$false,$true) 
-Show-MultiSelectMenu (Get-ChildItem -Path . -Directory | Select-Object -ExpandProperty FullName)  -selected @($true,$false,$true) 
-Show-BoxMenu @('first','second, very long option','third','fourth','fifth') -border $doubleBorder -Expand 
-Show-BoxMenu @('first','second option','third','fourth','fifth')  -selected @($true,$false,$true) -MultiSelect 
-Show-BoxMenu (Get-ChildItem -Path . -Directory | Select-Object -ExpandProperty FullName)  -selected @($true,$false,$true) -MultiSelect -Expand
+$lowASCIIBorder = '+-+|+++-+=' #are there any consoles or fonts where ASCII box borders won't show?
+$doubleBorder =   '╔═╗║╚╝╟─╢▒'
+#Show-SimpleMenu @('first','second option','third','fourth','fifth') -border $doubleBorder
+#Show-MultiSelectMenu @('first','second','third','fourth','fifth')  -selected @($true,$false,$true) 
+#Show-MultiSelectMenu (Get-ChildItem -Path . -Directory | Select-Object -ExpandProperty FullName)  -selected @($true,$false,$true) 
+#Show-BoxMenu @('first','second, very long option','third','fourth','fifth') -border $doubleBorder -Expand 
+#Show-BoxMenu @('first','second option','third','fourth','fifth')  -selected @($true,$false,$true) -MultiSelect 
+Show-BoxMenu (Get-ChildItem -Path . -file | Select-Object -ExpandProperty FullName)  -selected @($true,$false,$true) -MultiSelect -Expand 
