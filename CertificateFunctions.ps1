747a9e3c22b0cb607fc4a0af5c1c986c9f69a2c4
mossrich <35710431+mossrich@users.noreply.github.com>
2019-10-27 14:22:16 -0700

    Added Get-CredentialsFromAppPool and Get-CertFromUserStore
    Get-CredentialsFromAppPool - retrieves a PSCredential object from the specified IIS App Pool
    Get-CertFromUserStore - retrieves certs from the specified user's cert store

--- CertificateFunctions.ps1
+++ CertificateFunctions.ps1
@@ -18,8 +18,35 @@ function GetCertThumbprintFromStore{#Get a single matching cert, an exception if
   }
 } 
 
+Function Get-CredentialsFromAppPool([string] $AppPoolWithIdentity = 'Configuration'){#retrieves service account credential from IIS App Pool
+    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+        Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit #restart as admin if not started as admin 
+    }
+    Push-Location $env:TEMP
+    $UserName = cmd.exe /q /c "$env:windir\system32\inetsrv\appcmd.exe list apppool `"$AppPoolWithIdentity`" /text:ProcessModel.UserName"
+    $Password = cmd.exe /q /c "$env:windir\system32\inetsrv\appcmd.exe list apppool `"$AppPoolWithIdentity`" /text:ProcessModel.Password"
+    If(!$UserName) {Throw "No identity found for App Pool: $AppPoolWithIdentity"}
+    Pop-Location 
+    Return New-Object System.Management.Automation.PSCredential($UserName,(ConvertTo-SecureString $Password -AsPlainText -Force))
+}
 
+Function Get-CertFromUserStore(
+    [pscredential] $UserCredential,
+    [string] $SubjectSearchString = 'CN=*',
+    $CertificatePath = 'Cert:\CurrentUser\My'
+  ){
+    $job = Start-Job -Credential $UserCredential -ArgumentList $CertificatePath,$SubjectSearchString -ScriptBlock { Param([string] $CertPath, [string] $CertSubject)
+        Get-ChildItem -Path $CertPath | Where {$_.Subject -like $CertSubject -and $_.NotAfter -ge [datetime]::Now}
+    }
+    $job | Wait-Job | Out-Null
+    $out = ($job | Receive-Job)
+    $job | Remove-Job
+    Return $out
+}
 
+Get-CertFromUserStore (Get-CredentialsFromAppPool)
+<#
 $thumbprint = GetCertThumbprintFromStore -CertStoreName "Cert:\LocalMachine\My"
 $cert = (Get-ChildItem -path Cert: -Recurse | ? {$_.Thumbprint -eq $thumbprint})
 $cert | select * | Format-List
+#>
