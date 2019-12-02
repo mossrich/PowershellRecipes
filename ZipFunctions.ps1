acc3ce223100e2055553c0968ec6baaa7d9a02f1
mossrich <35710431+mossrich@users.noreply.github.com>
2019-12-02 00:31:02 -0800

    Parameters for in-memory manipulation of Zip files

--- ZipFunctions.ps1
+++ ZipFunctions.ps1
@@ -1,21 +1,10 @@
 @( 'System.IO.Compression','System.IO.Compression.FileSystem') | % { [void][System.Reflection.Assembly]::LoadWithPartialName($_) }
 
-function Add-ZipEntry([string] $ZipFilePath, [string] $EntryPath, [byte[]] $Content){
-    $FileStream = New-Object IO.FileStream($ZipFilePath, [IO.FileMode]::Open)
-    $ZipArchive = New-Object IO.Compression.ZipArchive($FileStream, [IO.Compression.ZipArchiveMode]::Update)
-    $Entry = $ZipArchive.CreateEntry($EntryPath)
-    $WriteStream = New-Object System.IO.StreamWriter($Entry.Open())
-    $WriteStream.Write($Content,0,$Content.Length)
-    $WriteStream.Flush()
-    $WriteStream.Dispose()
-    $ZipArchive.Dispose()
-    $FileStream.Close()
-    $FileStream.Dispose()
-}
-
-function Get-ZipEntryContent([string] $ZipFilePath, [string] $EntryPath){#returns the bytes of the first matching entry 
-    $FileStream = New-Object IO.FileStream($ZipFilePath, [IO.FileMode]::Open)
-    $ZipArchive = New-Object IO.Compression.ZipArchive($FileStream, [IO.Compression.ZipArchiveMode]::Read)
+function Get-ZipEntryContent(#returns the bytes of the first matching entry 
+  [string] $ZipFilePath, #optional - specify a ZipStream or path 
+  [IO.Stream] $ZipStream = (New-Object IO.FileStream($ZipFilePath, [IO.FileMode]::Open)),
+  [string] $EntryPath){
+    $ZipArchive = New-Object IO.Compression.ZipArchive($ZipStream, [IO.Compression.ZipArchiveMode]::Read)
     $buf = New-Object byte[] (0) #return an empty byte array if not found
     $ZipArchive.GetEntry($EntryPath) | ?{$_} | %{ #GetEntry returns first matching entry or null if there is no match
         $buf = New-Object byte[] ($_.Length)
@@ -23,28 +12,60 @@ function Get-ZipEntryContent([string] $ZipFilePath, [string] $EntryPath){#return
         $_.Open().Read($buf,0,$buf.Length)
     }
     $ZipArchive.Dispose()
-    $FileStream.Close()
-    $FileStream.Dispose()
+    $ZipStream.Close()
+    $ZipStream.Dispose()
     return $buf 
 }
 
-function Delete-ZipEntries([string] $ZipFilePath, [string] $WildcardNamesToDelete){#will delete all 
-    $FileStream = New-Object IO.FileStream($ZipFilePath, [IO.FileMode]::Open)
-    $ZipArchive    = New-Object IO.Compression.ZipArchive($FileStream, [IO.Compression.ZipArchiveMode]::Update)
+function Delete-ZipEntries(
+  [string] $ZipFilePath, #optional - specify a ZipStream or path 
+  [IO.Stream] $ZipStream = (New-Object IO.FileStream($ZipFilePath, [IO.FileMode]::Open)),
+  [string] $EntryPath,
+  [string] $WildcardNamesToDelete){
+    $ZipArchive = New-Object IO.Compression.ZipArchive($ZipStream, [IO.Compression.ZipArchiveMode]::Read)
     ($zip.Entries | ? { $_.Name -like $WildcardNamesToDelete }) | % { 
         Write-Verbose "     Deleting: $($_.Name)"
         $_.Delete() 
     }
     $ZipArchive.Dispose()
-    $FileStream.Close()
-    $FileStream.Dispose()
+    $ZipStream.Close()
+    $ZipStream.Dispose()
 }
 
-$zipPath = 'C:\temp\temp.zip'
-$fileInZip = 'folder/temp.csv'
+function Add-ZipEntry(#Adds an entry to the $ZipStream. Sample call: Add-ZipEntry -ZipFilePath "$PSScriptRoot\temp.zip" -EntryPath Test.xml -Content ([text.encoding]::UTF8.GetBytes("Testing"))
+  [string] $ZipFilePath, #optional - specify a ZipStream or path 
+  [IO.Stream] $ZipStream = (New-Object IO.FileStream($ZipFilePath, [IO.FileMode]::OpenOrCreate)),
+  [string] $EntryPath, 
+  [byte[]] $Content, 
+  [switch] $OverWrite, #if specified, will not create a second copy of an existing entry 
+  [switch] $PassThru ){#return a copy of $ZipStream
+    $ZipArchive = New-Object IO.Compression.ZipArchive($ZipStream, [IO.Compression.ZipArchiveMode]::Update, $true)
+    $ExistingEntry = $ZipArchive.GetEntry($EntryPath) | ?{$_} 
+    If($OverWrite -and $ExistingEntry){
+        Write-Verbose "    deleting existing $($ExistingEntry.FullName)"
+        $ExistingEntry.Delete()
+    }
+    $Entry = $ZipArchive.CreateEntry($EntryPath)
+    $WriteStream = New-Object System.IO.StreamWriter($Entry.Open())
+    $WriteStream.Write($Content,0,$Content.Length)
+    $WriteStream.Flush()
+    $WriteStream.Dispose()
+    $ZipArchive.Dispose()
+    If($PassThru){
+        $OutStream = New-Object System.IO.MemoryStream
+        $ZipStream.Seek(0, 'Begin') | Out-Null
+        $ZipStream.CopyTo($OutStream)
+    }
+    $ZipStream.Close()
+    $ZipStream.Dispose()
+    If($PassThru){$OutStream}
+}
 
-$OldContent = ([Text.Encoding]::ASCII).GetString((Get-ZipEntryContent -ZipFilePath $zipPath -EntryPath $fileInZip)) 
-$NewContent = $OldContent.Replace('FIND THIS','REPLACE WITH THIS')
+$path = "$PSScriptRoot\temp.zip"
+Add-ZipEntry  -ZipFilePath $path -EntryPath Test.xml -Content ([text.encoding]::UTF8.GetBytes("<xml><test>1</test>"))
+$bytes = Get-ZipEntryContent -ZipStream (New-Object IO.FileStream($path, [IO.FileMode]::Open)) -EntryPath 'Test.xml'
+[text.encoding]::UTF8.GetString($bytes)
 
-Delete-ZipEntries $zipPath -WildcardNamesToDelete '*.csv'
-Add-ZipEntry -ZipFilePath $zipPath -EntryPath $fileInZip -Content ([Text.Encoding]::ASCII).GetBytes($NewContent)
+$NewZipStream = Add-ZipEntry -ZipStream (New-Object IO.MemoryStream) -EntryPath Test.xml -Content ([text.encoding]::UTF8.GetBytes("<xml><test>1</test>")) -PassThru
+$bytes = Get-ZipEntryContent -ZipStream $NewZipStream -EntryPath 'Test.xml'
+[text.encoding]::UTF8.GetString($bytes)
