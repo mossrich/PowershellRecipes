63f4d2ed3c724113bc833de80a1b23202464a5ad
mossrich <35710431+mossrich@users.noreply.github.com>
2020-02-07 12:56:20 -0800

    Added Read-Host with default value
    Migrating to Read-Host from Windows.Forms input box because it frequently hangs ISE. 
    Added a one liner to allow user entry but also a default value with Read-host.

--- Choose.ps1
+++ Choose.ps1
@@ -1,10 +1,16 @@
-$choices = [System.Management.Automation.Host.ChoiceDescription[]](
-    (new-Object System.Management.Automation.Host.ChoiceDescription "&Restart","Restart"), #Label,HelpMessage
-    (new-Object System.Management.Automation.Host.ChoiceDescription "&ShutDown","Shutdown")
-)
-$answer =  $Host.ui.PromptForChoice("Choose Action","What do you want to do?",$choices,0)
+#inspired by https://stackoverflow.com/questions/26386267/is-there-a-one-liner-for-using-default-values-with-read-host
+'Default Value' | %{If($Entry = Read-Host "Enter a value ($_)"){$Entry}Else{$_}} #returns user entry or 'Default Value' if user hit [Enter] 
 
+$choices = [Management.Automation.Host.ChoiceDescription[]](
+    (New-Object Management.Automation.Host.ChoiceDescription "&localhost","127.0.0.1"), #Label,HelpMessage
+    (New-Object Management.Automation.Host.ChoiceDescription "&Google","www.google.com")
+)
+$Chosen = $choices[$Host.ui.PromptForChoice("Choose host","What host do you want to use?",$choices,0)].HelpMessage
+Write-Host $Chosen
+
+<#
 switch ($answer){
     0 {"You chose $($choices[$answer].Label)"; break}
     1 {"You chose $($choices[$answer].Label)"; break}
 }
+#>
