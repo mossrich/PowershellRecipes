65830a980a1323ccdf0bd62267b1a217121cee0e
mossrich <35710431+mossrich@users.noreply.github.com>
2024-12-12 15:29:38 -0800

    Update SqlDBBrowser.ps1 - code cleanup and adding ListView
    Cleaned up all commented code that used a DataGrid. 
    A multi-column ListView is better than the previous (hacky) way I separated the table name, row count and column names with parentheses. 
    FillListViewFromDataTable is reusable. Later I might add some other type-based formatting besides just numeric-right-aligned.

--- SqlDBBrowser.ps1
+++ SqlDBBrowser.ps1
@@ -1,16 +1,18 @@
 #Shows a list of user SQL databases for the local connection in a ComboBox
 #Choose a database from the drop-down and all the tables with data will be listed. 
-#Double-click a table and the top 100 rows will be shown in the grid
+#Double-click a table and the top 100 rows will be shown in the grid with similar cells merged
 Param(
+    $ConnectionString = 'Data Source=.;Integrated Security=SSPI;',#TODO: get connections from a config file
     $DBScript = "SELECT name FROM master.sys.databases WHERE name NOT IN ('master','tempdb','model','msdb')",
     $TableListScript = @"
         USE {0}
-        SELECT '{0}.' + s.NAME + '.[' + t.Name + '] (' + CONVERT(varchar(50),p.rows) + ')' [Name] 
+        SELECT '{0}.' + s.NAME + '.[' + t.Name + ']' [TableName], p.Rows, 
+         STUFF((SELECT ', ' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS C 
+         WHERE C.TABLE_SCHEMA = s.Name AND C.TABLE_NAME = t.Name ORDER BY ORDINAL_POSITION FOR XML PATH('')),1,1,'') [Columns]
         FROM sys.tables t JOIN sys.indexes i ON t.OBJECT_ID = i.object_id JOIN sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id LEFT OUTER JOIN sys.schemas s ON t.schema_id = s.schema_id 
-        WHERE t.NAME NOT LIKE 'dt%' AND t.is_ms_shipped = 0 AND i.OBJECT_ID > 255 {1} GROUP BY s.Name, t.Name, p.Rows ORDER BY s.Name, t.Name
+        WHERE t.NAME NOT LIKE 'dt%' AND t.is_ms_shipped = 0 AND i.OBJECT_ID > 255 {1} GROUP BY s.Name, t.Name, p.Rows ORDER BY 1 
 "@,
-    $TableContentScript = "SELECT TOP ({0}) * FROM {1} ORDER BY 1",
-    $ConnectionString = 'Data Source=.;Integrated Security=SSPI;',#TODO: get connections from SSMS MRU
+    $TableContentScript = "SELECT TOP ({0}){1} `r`n FROM {2} ORDER BY 1",
     $DefaultMaxRows = 100 #this can be overridden in the SQL text box
 )
 
@@ -24,46 +26,59 @@ Function RunSqlQuery($QueryText, $ConnString = $ConnectionString){#Write-Host $Q
       $conn.Close()
       return $ds.Tables
 }
-Function FillTableList(){
+Function FillListViewFromDataTable([Data.DataTable] $tbl, [Windows.Forms.ListView] $lv){
+    $lv.Columns.Clear();$lv.Items.Clear()
+    $NumericColumnTypes = 'Decimal','Double','Int16','Int32','Int64','Single','UInt16','UInt32','UInt64' #All Types: Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 SByte Single String TimeSpan UInt16 UInt32 UInt64 
+    $tbl.Columns | %{
+        $newCol = $lv.Columns.Add($_.ColumnName)
+        If($NumericColumnTypes.Contains($_.DataType.Name)){$NewCol.TextAlign = 'Right'} #Numeric columns will be right-aligned
+    }
+    Foreach($row in $tbl.Rows){ 
+        $LvItem = New-Object Windows.Forms.ListViewItem($row[0])
+        1..($row.ItemArray.Count-1) | %{ $LvItem.SubItems.Add($row[$_]) }
+        $lv.Items.Add($LvItem)
+    }
+}
+Function FillTableList(){#when a DB is selected fill the list with its tables
     If($ExcludeEmptyCheckBox.Checked){$Filter = " AND p.rows>0 "} Else {$Filter= ""}
-    $TableList.Datasource = [Collections.ArrayList]((RunSqlQuery ($TableListScript -f $DBCombo.Text,$Filter))[0].Rows | %{$_.Name })  
+    FillListViewFromDataTable (RunSqlQuery ($TableListScript -f $DBCombo.Text,$Filter))[0] $TableList
+    $TableList.AutoResizeColumn(0,'ColumnContent')#Resize the TableName column
+    If($TableList.Items.Count -ge 0){$TableList.Items[0].Selected = $true;$TableList.Items[0].Focused = $true}
 }
 
 $frm = New-Object Windows.Forms.Form -Property @{Text = 'SQL DB Browser';Size = '1200,800';add_Shown={$DBCombo.Focus(); FillTableList}}#TODO: raise selection changed event
 $SplitContainerAll = New-Object Windows.Forms.SplitContainer -Property @{Dock='Fill';Orientation='Horizontal';} 
 $SplitContainerTop = New-Object Windows.Forms.SplitContainer -Property @{Dock='Fill';Orientation='Horizontal';SplitterDistance = 15;FixedPanel = 'Panel1'} 
 $SplitContainerMiddle = New-Object Windows.Forms.SplitContainer -Property @{Dock='Fill';Orientation='Vertical';} 
-
 $DBCombo = New-Object Windows.Forms.ComboBox -Property @{Size='330,20';Location='0,0'
     DataSource = [Collections.ArrayList]((RunSqlQuery $DBScript)[0].Rows | %{$_.Name })
     add_SelectedIndexChanged = { FillTableList }
 }
-$RunButton = New-Object Windows.Forms.Button -Property @{Size='40,20';Location='530,0';Text='&Run'; #Run with Alt-R
+$RunButton = New-Object Windows.Forms.Button -Property @{Size='40,20';Location='600,0';Text='&Run'; #Run with Alt-R
     add_Click = { 
         If($SqlTextBox.SelectionLength -gt 0){$SQL=$SqlTextBox.SelectedText} Else {$SQL=$SqlTextBox.Text} 
-        $dg.DataSource = (RunSqlQuery $SQL)[0]  
+        $dg.DataSource = (RunSqlQuery $SQL)[0]   
     } 
 }
 $ExcludeEmptyCheckBox = New-Object Windows.Forms.CheckBox -Property @{Checked=$true;Location='350,0';Size='100,30';Text = 'Exclude Empty'
     add_Click = {$DBCombo.Focus(); FillTableList}
 }
-$TableList = New-Object Windows.Forms.ListBox -Property @{ScrollAlwaysVisible=$true;Location='0,0';Dock='Fill'
-    add_SelectedIndexChanged = {$SqlTextBox.Text = ($TableContentScript -f $DefaultMaxRows, $TableList.SelectedValue.Split('(')[0] ) }
+$TableList = New-Object Windows.Forms.ListView -Property @{Location='0,0';Dock='Fill';#MultiColumn = $true;#ScrollAlwaysVisible=$true;
+    FullRowSelect = $true;View = 'Detail';GridLines = $true
+    add_SelectedIndexChanged = {Param([Windows.Forms.ListView] $sender, $e) 
+        If($sender.SelectedItems){
+            $SqlTextBox.Text = ($TableContentScript -f $DefaultMaxRows, $sender.SelectedItems[0].SubItems[2].Text, $sender.SelectedItems[0].Text)
+        }
+    }
     add_DoubleClick = {$RunButton.PerformClick() }
 }
-$SqlTextBox = New-Object Windows.Forms.TextBox -Property @{ScrollBars='Vertical';Multiline=$true;Dock='Fill';#Location='0,0';
+$SqlTextBox = New-Object Windows.Forms.TextBox -Property @{ScrollBars='Vertical';Multiline=$true;Dock='Fill';Font='Lucida Console'#Location='0,0';
     add_KeyUp = {Param($sender,[Windows.Forms.KeyEventArgs]$e) 
-        If($e -eq [Windows.Forms.Keys]::F5){ $tbl = RunSqlQuery $SqlTextBox.Text;  $dg.DataSource = $tbl[0]} 
+        If($e.KeyCode -eq [Windows.Forms.Keys]::F5){ $RunButton.PerformClick() } 
     }
 }
 $dg = New-Object Windows.Forms.DataGridView -Property @{Dock='Fill';
-     <# Uncomment to use the simpler DataGrid - it needs auto-resizing to be useful. 
-     AllowSorting=$true;HeaderForeColor=[Drawing.Color]::FromArgb(255,0,0,0)
-     add_DatasourceChanged = { Param ($sender, $e)  # auto-resize columns - http://www.hanselman.com/blog/HowDoIAutomaticallySizeAutosizeColumnsInAWinFormsDataGrid.aspx  https://www.codeproject.com/articles/5429/auto-sizing-datagrid
-         $ColAutoResizeMethod = $sender.GetType().GetMethod('ColAutoResize',('static','nonpublic','instance') ) #get the private method
-         For ([int]$i = $sender.FirstVisibleColumn; $i -lt $sender.VisibleColumnCount; $i++){ $ColAutoResizeMethod.Invoke($sender, $i) | Out-Null }
-     }#>
-     AutoSizeColumnsMode = 'AllCells';AutoSizeRowsMode = 'None' #$dg.DefaultCellStyle.Padding.All = 0  - doesn't remove padding :( 
+     AutoSizeColumnsMode = 'AllCells';AutoSizeRowsMode = 'None' #$dg.DefaultCellStyle.Padding.All = 0  - doesn't remove padding :(
      add_CellPainting = {Param([Windows.Forms.DataGridView] $sender, [Windows.Forms.DataGridViewCellPaintingEventArgs] $e)  #merge rows
         $e.AdvancedBorderStyle.Bottom = [Windows.Forms.DataGridViewAdvancedCellBorderStyle]::None #TODO: set cell template with these borders - need to create an inherited dgview
         Try{
@@ -91,4 +106,5 @@ $SplitContainerTop.Panel2.Controls.Add($SplitContainerMiddle)
 $SplitContainerAll.Panel1.Controls.Add($SplitContainerTop)
 $SplitContainerAll.Panel2.Controls.Add($dg)
 $frm.Controls.AddRange(@($SplitContainerAll))
-[windows.forms.application]::run($frm)
+$SplitContainerMiddle.SplitterDistance = 600
+[windows.forms.application]::run($frm) #$frm.ShowDialog()
