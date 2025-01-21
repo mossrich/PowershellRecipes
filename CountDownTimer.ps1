ddacc96d98ef6422060be0f10d7966f5b42fb241
mossrich <35710431+mossrich@users.noreply.github.com>
2025-01-20 22:17:30 -0800

    Update CountDownTimer.ps1
    Added terse version

--- CountDownTimer.ps1
+++ CountDownTimer.ps1
@@ -5,6 +5,10 @@
 #start-sleep (10); write-host ("`a"*4)
 for([int] $i=$SecondsToCountDown;$i -gt 0;$i--){ Write-Progress "Starting in a few seconds" -PercentComplete ((($SecondsToCountDown - $i)/$SecondsToCountDown) * 100)   ;Start-Sleep 1; }
 
+
+#terse version
+$secs=5; $secs..0|%{Write-Progress "Starting in $_ seconds" -PercentComplete ((($secs - $_)/$secs) * 100); Start-Sleep 1}
+
 $input = $(
       Add-Type -AssemblyName Microsoft.VisualBasic
       [Microsoft.VisualBasic.Interaction]::InputBox('Enter your city','Titlebar Text', 'Default new york')
